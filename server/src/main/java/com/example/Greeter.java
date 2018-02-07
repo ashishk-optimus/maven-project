5b5d15c980a5b33a5b6228580a8046dd873c438b
AshishKumar <ashish.kumar@OPTIMUSDOM.COM>
2018-02-07 17:49:50 +0530

    3rd Change

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,8 +11,13 @@ public class Greeter {
   public Greeter() {
 
   }
+  
+  /**
+   * @param someone a person name
+   * @return greeting string
+   */
 
-  public String greet(final String someone) {
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
